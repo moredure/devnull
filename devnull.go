779375c280f1fd3dc633c44f0898a186c875f3ae
Mike Faraponov <11322032+mikefaraponov@users.noreply.github.com>
2019-07-30 20:19:31 +0300

    Update devnull.go

--- devnull.go
+++ devnull.go
@@ -1,9 +1,8 @@
 package devnull
 
-type reader struct{
-}
+type reader struct{}
 
-func (*reader) Read(b []byte) (int, error) {
+func (reader) Read(b []byte) (int, error) {
 	for i := range b {
 		b[i] = 0
 	}
@@ -12,8 +11,23 @@ func (*reader) Read(b []byte) (int, error) {
 
 var Reader *reader
 
-func init() {
-  Reader = new(reader)
+func Read(b []byte) (n int, err error) {
+	return Reader.Read(b)
 }
 
-func Read(b []byte) { Reader.Read(b) }
+var Writer *writer
+
+type writer struct{}
+
+func (writer) Write(p []byte) (n int, err error) {
+	return len(p), nil
+}
+
+func Write(p []byte) (n int, err error) {
+	return Writer.Write(p)
+}
+
+func init() {
+	Reader = new(reader)
+	Writer = new(writer)
+}
